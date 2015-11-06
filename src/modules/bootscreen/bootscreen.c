db2f181a2864aa25dffefcc8d249440940bd2f01
Moritz Grosch (LittleFox) <littlefox@fsfe.org>
2015-11-06 21:55:07 +0100

    Created a nice "bootscreen"

--- src/modules/bootscreen/bootscreen.c
+++ src/modules/bootscreen/bootscreen.c
@@ -2,6 +2,23 @@ void module_init_bootscreen() {
     unsigned short *vga = (unsigned short*)0xB8000;
 
     for(int i = 0; i < 80*25; i++) {
-        vga[i] = 'c' | (0x07 << 8);
+        vga[i] = ' ' | (0x00 << 8);
+    }
+
+    char* hello_message = "LF OS - the 4th attempt\n"
+                          "========================\n\n"
+                          "Booting ...";
+
+    int x = 0, y = 0;
+    char c;
+    while((c = *(hello_message++))) {
+        if(c == '\n') {
+            x = 0;
+            y++;
+            continue;
+        }
+
+        vga[(y * 80) + x] = c | (0x0e << 8);
+        x++;
     }
 }
