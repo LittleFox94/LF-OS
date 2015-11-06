373f510ce55bd73724ad3da1be543cfaf0f99add
Moritz Grosch (LittleFox) <littlefox@fsfe.org>
2015-11-06 21:53:14 +0100

    fixed module-collection calling

--- src/kernel/common/main.cpp
+++ src/kernel/common/main.cpp
@@ -15,8 +15,15 @@ void kernelMain(char *ptrInitrd, int initrdLength)
     struct module_collection* modules = (struct module_collection*)magic_pointer;
 
     if(modules->version_major != 0 || modules->version_minor != 1) {
-        archPanic("Miep");
+        archPanic("Invalid module collection version");
     }
+
+    if(modules->checksum != -(modules->magic + modules->version_major + modules->version_minor + modules->num_modules)) {
+        archPanic("Module collection checksum is invalid");
+    }
+
+    // everything is right, copy it to the right place
+    memcpy((char*)0x8000000, (char*)magic_pointer, initrdLength);
 
     for(int step = 1; step < 12; step++) {
         for(int i = 0; i < modules->num_modules; i++) {
